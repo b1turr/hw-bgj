633fa81a34e996aef4ab6595b039c1c1b620ddf5
batur1 <xozkb001@studenti.czu.cz>
2025-10-16 13:13:57 +0200

    Refactor salaryTest to use constructor and methods

--- hw_Java/src/hw02/OOP beginner projects/salaryTest.java
+++ hw_Java/src/hw02/OOP beginner projects/salaryTest.java
@@ -1,18 +1,12 @@
+package hw02.OOP.projects;
+
 public class salaryTest {
     public static void main(String[] args) {
-    salary marksyearSalary = new salary(); // creating object for annual salary and its method
-    marksyearSalary.monthlySalary = 30000; // declaration of monthly salary
-    marksyearSalary.math = marksyearSalary.monthlySalary*12; // total salary information to math variable
-
-    int totalyearmark = marksyearSalary.getYearlySalary(marksyearSalary.math);
-        System.out.println("Mark's total annual salary is: " + totalyearmark);
-
-    salary marksdoubleSalary = new salary(); // creating object for double salary and its method
-    marksdoubleSalary.monthlySalary = 30000; // declaration of monthly salary
-    marksyearSalary.math = marksdoubleSalary.monthlySalary*2; // double salary declaration to math
-    int doublemark = marksdoubleSalary.getDoubleSalary(marksyearSalary.math);
-        System.out.println("Mark's double salary is: " + doublemark);
-
-
+        salary marksSalary = new salary(3000);
+        int result_year = marksSalary.getYearlySalary();
+        System.out.println("Mark's annual salary: " + result_year);
+        int result_double = marksSalary.getDoubleSalary();
+        System.out.println("Mark's double salary: " + result_double);
     }
 }
+
