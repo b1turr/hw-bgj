123ff4750133bf3dd7684e65f656c57769adab52
batur1 <baturayozkara123@gmail.com>
2025-10-18 12:38:29 +0200

    Fix class name case
    I've got information about how should I use name conventions in Java for classes

--- hw_Java/src/hw02/OOP beginner projects/Car.java
+++ hw_Java/src/hw02/OOP beginner projects/Car.java
@@ -1,11 +1,12 @@
-public class car {
+public class Car {
     String model;
     String brand;
     String year;
-
     String secondHandSell(String price){
+      
         return "Car's brand is: " + brand + "model is: " + model + "Year: " + year + "Total price => " + price;
     }
 
 }
+
 
