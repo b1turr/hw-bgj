0257ca1766ffd2fd90649b8959b3eccf306fe1b7
batur1 <baturayozkara123@gmail.com>
2025-10-18 12:39:46 +0200

    Update and rename carTest.java to CarTest.java
    I've got information about how should I use name conventions in Java for classes

--- hw_Java/src/hw02/OOP beginner projects/CarTest.java
+++ hw_Java/src/hw02/OOP beginner projects/CarTest.java
@@ -1,8 +1,8 @@
-public class carTest {
+public class CarTest {
     static void main() {
         car car1 = new car(); // creating first object
         car1.brand = "Audi\n";
-        car1.model = "a4\n";
+        car1.model = "A4\n";
         car1.year = "2016\n";
 
         String price = car1.secondHandSell("130.000czk\n"); // giving input to argument of secondHandSell object
@@ -11,3 +11,4 @@ public class carTest {
     }
 
 }
+
